71aba06613bde875f5f687b4298bef80873481a1
Aleksandr-Faleev <sany1997.007@gmail.com>
2024-10-30 01:21:27 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Pers vasy = new Pers("vasy", "фотограф", "маил", "номер", 42000, 32);
+        Emploee vasy = new Emploee("vasy", "фотограф", "маил", "номер", 42000, 32);
         vasy.print();
+
+
+        }
     }
-}
